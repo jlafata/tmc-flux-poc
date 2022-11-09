400b37942bb01c6b5c5a230f663dcbb1bbc8ccc6
John LaFata <jlafata@vmware.com>
2022-11-09 10:29:47 -0500

    set setcrets in postgres-instance domain and add unmanaged postgres instance database

---
+++ postgres/set-secrets.sh
@@ -0,0 +1,2 @@
+../../deployment_credentials/postgres/regsecrets/registry-tanzu-vmware-com/regsecret.sh postgres-instances
+../../deployment_credentials/postgres/regsecrets/jltestcr-azurecr-io/regsecret.sh postgres-instances
--- postgres/setup-resource-claims-postgres.sh
+++ postgres/setup-resource-claims-postgres.sh
@@ -1,3 +1,4 @@
+./setup-secrets.sh
 kubectl apply -f resource-claims-cluster-role-postgres.yaml
 kubectl apply -f cluster-instance-class-postgres.yaml
 kubectl apply -f resource-claim-policy-postgres.yaml
