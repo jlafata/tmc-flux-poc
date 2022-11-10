ef78dddd6127816182e7f281b29e0a663a738946
John LaFata <jlafata@vmware.com>
2022-11-10 06:33:02 -0500

    updates app-deployment info

---
+++ kustomize/set-secrets.sh
@@ -0,0 +1,4 @@
+../deployment_credentials/postgres/regsecrets/registry-tanzu-vmware-com/regsecret.sh postgres-instances
+../deployment_credentials/postgres/regsecrets/jltestcr-azurecr-io/regsecret.sh postgres-instances
+../deployment_credentials/postgres/regsecrets/registry-tanzu-vmware-com/regsecret.sh dev-space
+../deployment_credentials/postgres/regsecrets/jltestcr-azurecr-io/regsecret.sh dev-space
--- postgres/setup-resource-claims-postgres.sh
+++ postgres/setup-resource-claims-postgres.sh
@@ -1,4 +1,4 @@
-./setup-secrets.sh
+./set-secrets.sh
 kubectl apply -f resource-claims-cluster-role-postgres.yaml
 kubectl apply -f cluster-instance-class-postgres.yaml
 kubectl apply -f resource-claim-policy-postgres.yaml
